c6784f5a77897a70d748b5735f4bfe825a013400
isimmons <isimmons33@gmail.com>
2024-02-10 02:46:55 -0500

    mock module currency test getPriceInCurrency

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,4 +1,12 @@
 import { it, expect, describe, vi } from "vitest";
+import { getPriceInCurrency } from "../src/mocking";
+import { getExchangeRate } from "../src/libs/currency";
+
+// this will mock all exported functions from the currency module
+// this line is hoisted so it replaces the functions with the mocked
+// version first. So, when getExchangeRate is imported above
+// it is actually the mocked version being imported
+vi.mock("../src/libs/currency");
 
 describe("greet", () => {
   it("should mock the greet function", async () => {
@@ -23,5 +31,20 @@ describe("sendText", () => {
     const result = sendText("message");
     expect(sendText).toHaveBeenCalledWith("message");
     expect(result).toBe("ok");
+  });
+});
+
+describe("getPriceInCurrency", () => {
+  it("should return price in target currency", () => {
+    // pass reference to the getExchangeRate function and set
+    // the mocked return value.
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+    // now whenever getExchangeRate is called it will return 1.5
+    // even when getPriceInCurrency calls it
+    // remember getPriceInCurrency is the function we are testing
+    // we have to mock anything it needs externally in order to do it's job
+    const price = getPriceInCurrency(10, "AUD");
+
+    expect(price).toBe(15);
   });
 });
